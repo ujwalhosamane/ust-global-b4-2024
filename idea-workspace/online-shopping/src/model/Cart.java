875f53a3034d46af36bf7bb2e265b76fa20d97f2
H U Ujwal <77206767+ujwalhosamane@users.noreply.github.com>
2024-06-15 19:53:50 +0530

    Update Cart.java

--- idea-workspace/online-shopping/src/model/Cart.java
+++ idea-workspace/online-shopping/src/model/Cart.java
@@ -2,9 +2,28 @@ package model;
 
 public class Cart {
 
-    private Product []products;
+    private Product[] products;
 
     private float totalValue;
 
+    public Cart(Product[] products, float totalValue) {
+        this.products = products;
+        this.totalValue = totalValue;
+    }
 
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public void setProducts(Product[] products) {
+        this.products = products;
+    }
+
+    public float getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(float totalValue) {
+        this.totalValue = totalValue;
+    }
 }
