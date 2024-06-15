0666ebdf448a3852bc9f07405f868cda80ec7cc3
H U Ujwal <77206767+ujwalhosamane@users.noreply.github.com>
2024-06-15 19:51:20 +0530

    Update Product.java

--- idea-workspace/online-shopping/src/model/Product.java
+++ idea-workspace/online-shopping/src/model/Product.java
@@ -7,4 +7,42 @@ public class Product {
     private String description;
     private float price;
 
+    public Product(int productId, String productName, String description, float price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return this.productId;
+    }
+
+    public String getProductName() {
+        return this.productName;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
